f99d605c190da63e568ecad5cb1e544f5cc6eb05
liam <liam.neufeld@sasktel.net>
2020-10-22 21:33:07 -0700

    Added healthbar sprites and scripts

---
+++ Assets/Scripts/Player/healthBarBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBarBackground : MonoBehaviour
+{
+    Vector3 localScale;
+
+    void Start()
+    {
+        localScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        localScale.x = Player.maxHealth / 50;
+        transform.localScale = localScale;
+    }
+}
