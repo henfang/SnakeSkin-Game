a65e57d0fbfc3e7c11a650c7d5690131cbe59bf1
PC-PC\PC <go6ko01@yahoo.com>
2020-11-01 12:36:41 -0600

    Add SpeedBuff, and Toggle Inventory

---
+++ Assets/Scripts/Items/PickUp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [Range(0, 20)]
+    public float speedBuff;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Player.instance.SpeedUp(speedBuff);
+            Destroy(gameObject);
+        }
+    }
+}
