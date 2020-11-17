51588270da30f718d05ad653d77424f50afb60cb
Deoni <del445@mail.usask.ca>
2020-11-16 21:44:05 -0600

    Touch-ups: Identifying the map and objects as the floor generates, Big Green doesn't rotate around, colour changes

--- Assets/Scripts/Enemies/Pathfinding/AstarScan.cs
+++ Assets/Scripts/Enemies/Pathfinding/AstarScan.cs
@@ -7,7 +7,12 @@ public class AstarScan : MonoBehaviour
 {
     IEnumerator Start()
  {
+     // scan inital surroundings
      yield return new WaitForSeconds(0.5f);
+     AstarPath.active.Scan();
+
+     // scan rest of surroundings
+     yield return new WaitForSeconds(5f);
      AstarPath.active.Scan();
  }
 }
