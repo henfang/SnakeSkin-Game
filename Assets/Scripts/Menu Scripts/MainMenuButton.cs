247852f9980562631c3a2171b425d762a3dd444c
liam <liam.neufeld@sasktel.net>
2020-11-18 16:19:49 -0600

    Added Menus

---
+++ Assets/Scripts/Menu Scripts/MainMenuButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour, IPointerDownHandler
+{
+    public Button menuButton;
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Menu Button Pressed.");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
