175e3f38af4dd4314d0ca230378eb67fa837f8e8
ClamBakery <hfang49@yahoo.ca>
2020-11-22 01:23:39 -0600

    Merge conflict solved

--- Assets/Scripts/Enemies/Pathfinding/AstarScan.cs
+++ Assets/Scripts/Enemies/Pathfinding/AstarScan.cs
@@ -8,16 +8,8 @@ public class AstarScan : MonoBehaviour
     public float waitTime = 5.0f;
     IEnumerator Start()
  {
-<<<<<<< HEAD
      yield return new WaitForSeconds(waitTime);
-=======
-     // scan inital surroundings
-     yield return new WaitForSeconds(0.5f);
->>>>>>> origin/master
      AstarPath.active.Scan();
 
-     // scan rest of surroundings
-     yield return new WaitForSeconds(5f);
-     AstarPath.active.Scan();
  }
 }
